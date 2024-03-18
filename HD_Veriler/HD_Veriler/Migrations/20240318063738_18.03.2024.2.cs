d0d85429317cb4157691a50ea02c23b8b37b254f
Baran Daşdemir <barandasdemir.bd@gmail.com>
2024-03-18 09:40:28 +0300

    18.03.2024

---
+++ HD_Veriler/HD_Veriler/Migrations/20240318063738_18.03.2024.2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HD_Veriler.Migrations
+{
+    /// <inheritdoc />
+    public partial class _180320242 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Question",
+                table: "Scores");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Question",
+                table: "Scores",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
